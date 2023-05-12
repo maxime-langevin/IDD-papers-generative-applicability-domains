6ae4b89a6e6f4107c3c927a9d624a430cb90555e
maxime-langevin <maxime.langevin@polytechnique.edu>
2023-05-12 17:59:34 +0200

    add percentiles to range-based AD

--- applicability_domains.py
+++ applicability_domains.py
@@ -11,28 +11,31 @@ from rdkit.Chem import AllChem
 from featurizers import ecfp4
 import Levenshtein
 
-class AD():
+
+class AD:
     """Each applicability domain class inherits from the AD class.
-       It must be initialized with a list of SMILES and a type of featurization, and 
-       implement a check_smiles_list method, that takes as input a list of SMILES,
-       and outputs a list of 0 and 1 according to whether each SMILES correspond to
-       a molecule within the AD"""
+    It must be initialized with a list of SMILES and a type of featurization, and
+    implement a check_smiles_list method, that takes as input a list of SMILES,
+    and outputs a list of 0 and 1 according to whether each SMILES correspond to
+    a molecule within the AD"""
+
     def __init__(self, reference_molecules, featurization):
         self.ref = reference_molecules
         self.featurization = featurization
 
+
 class convex_hull(AD):
     """Check whether a molecule is inside the convex hull of the reference set"""
-    def __init__(self, reference_molecules, featurization): 
+
+    def __init__(self, reference_molecules, featurization):
         super().__init__(reference_molecules, featurization)
         self.table = self.featurization(self.ref)
         variations = []
         values = []
         for i in range(len(np.array(self.table[0]))):
-            
             erase = False
             for element in self.table:
-                if np.array(element)[i]!=np.array(self.table[0])[i]:
+                if np.array(element)[i] != np.array(self.table[0])[i]:
                     erase = True
             if not erase:
                 variations.append(1)
@@ -47,17 +50,16 @@ class convex_hull(AD):
     def check_smiles_list(self, smiles_list):
         is_in_convex_hull = []
         for smiles in smiles_list:
-            
             X = np.array(self.featurization([smiles])[0])
-            
-            res = 1 - min(1, np.dot(self.variations, X!=self.values))
-            
-         
+
+            res = 1 - min(1, np.dot(self.variations, X != self.values))
+
             is_in_convex_hull.append(res)
-       
+
         return is_in_convex_hull
 
-def jaccard(im1, im2):   
+
+def jaccard(im1, im2):
     im1 = np.asarray(im1).astype(np.bool)
     im2 = np.asarray(im2).astype(np.bool)
 
@@ -68,6 +70,7 @@ def jaccard(im1, im2):
 
     union = np.logical_or(im1, im2)
     return intersection.sum() / float(union.sum())
+
 
 class similarity_max(AD):
     def __init__(self, reference_molecules, featurization, threshold=0.5):
@@ -80,52 +83,57 @@ class similarity_max(AD):
         is_in_convex_hull = []
         for smiles in smiles_list:
             if Chem.MolFromSmiles(smiles):
-                X = np.array(self.featurization([smiles])[0]) 
+                X = np.array(self.featurization([smiles])[0])
                 score = max([jaccard(X, y) for y in self.table])
-                is_in_convex_hull.append(1 * score>self.threshold)
+                is_in_convex_hull.append(1 * score > self.threshold)
             else:
                 is_in_convex_hull.append(0)
         return is_in_convex_hull
+
 
 class SMILESvalidity(AD):
     def __init__(self, reference_molecules, featurization):
         super().__init__(reference_molecules, featurization)
         self.name = "smiles_validity"
+
     def check_smiles_list(self, smiles_list):
         is_in_convex_hull = []
         for smiles in smiles_list:
             if Chem.MolFromSmiles(smiles):
                 is_in_convex_hull.append(1)
             else:
-                is_in_convex_hull.append(0) 
+                is_in_convex_hull.append(0)
         return is_in_convex_hull
+
 
 class filtersvalidity(AD):
     def __init__(self, reference_molecules, featurization):
         super().__init__(reference_molecules, featurization)
         self.name = "filters_validity"
         alert_collection_path = "data/alerts.csv"
-        
+
         names = []
         smarts = []
 
         with open(alert_collection_path) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
+            csv_reader = csv.reader(csv_file, delimiter=",")
             for i, row in enumerate(csv_reader):
-                if i>0:
+                if i > 0:
                     names.append(row[2])
                     smarts.append(row[3])
         names_already_present = []
-        for smiles in reference_molecules: 
+        for smiles in reference_molecules:
             for i, motif in enumerate(smarts):
                 subs = Chem.MolFromSmarts(motif)
-                if subs != None and Chem.MolFromSmiles(smiles).HasSubstructMatch(subs) and names[i] not in names_already_present:
+                if (
+                    subs != None
+                    and Chem.MolFromSmiles(smiles).HasSubstructMatch(subs)
+                    and names[i] not in names_already_present
+                ):
                     names_already_present.append(names[i])
         self.names = names
         self.smarts = smarts
-        self.names_already_present = names_already_present 
-
-            
+        self.names_already_present = names_already_present
 
     def check_smiles_list(self, smiles_list):
         is_in_convex_hull = []
@@ -134,7 +142,11 @@ class filtersvalidity(AD):
                 to_keep = True
                 for i, motif in enumerate(self.smarts):
                     subs = Chem.MolFromSmarts(motif)
-                    if subs != None and self.names[i] not in self.names_already_present and Chem.MolFromSmiles(smiles).HasSubstructMatch(subs):
+                    if (
+                        subs != None
+                        and self.names[i] not in self.names_already_present
+                        and Chem.MolFromSmiles(smiles).HasSubstructMatch(subs)
+                    ):
                         to_keep = False
                 if to_keep:
                     is_in_convex_hull.append(1)
@@ -143,6 +155,7 @@ class filtersvalidity(AD):
             else:
                 is_in_convex_hull.append(0)
         return is_in_convex_hull
+
 
 class levenshtein(AD):
     def __init__(self, reference_molecules, featurization, threshold=0.8):
@@ -156,33 +169,27 @@ class levenshtein(AD):
         for smiles in smiles_list:
             if Chem.MolFromSmiles(smiles):
                 score = max([Levenshtein.ratio(smiles, y) for y in self.table])
-                is_in_AD.append(1 * score>self.threshold)
+                is_in_AD.append(1 * score > self.threshold)
             else:
                 is_in_AD.append(0)
         return is_in_AD
 
 
 class in_range(AD):
-    def __init__(self, reference_molecules, featurization, tolerance=0):
+    def __init__(
+        self,
+        reference_molecules,
+        featurization,
+        tolerance=0,
+        lower_percentile=0.1,
+        upper_percentile=99.9,
+    ):
         super().__init__(reference_molecules, featurization)
         self.table = self.featurization(self.ref)
-        self.mins = []
-        self.maxs = []
-        
-        for i in range(len(np.array(self.table[0]))):
-            minimum = 1000
-            maximum = -1000
-           
-            for element in self.table:
-               
-                if element[i]>maximum:
-                    maximum = np.array(element)[i]
-                if element[i]<minimum:
-                    minimum = np.array(element)[i]
-               
-            self.mins.append(minimum)
-            self.maxs.append(maximum)
-           
+
+        self.lower = np.percentile(np.array(self.table), lower_percentile, axis=0)
+        self.upper = np.percentile(np.array(self.table), upper_percentile, axis=0)
+
         self.tolerance = tolerance
         self.name = "range"
 
@@ -193,13 +200,12 @@ class in_range(AD):
             try:
                 if Chem.MolFromSmiles(smiles):
                     X = np.array(self.featurization([smiles])[0])
-               
+
                     for i in range(len(X)):
-                    
-                        if X[i]>self.maxs[i] or X[i]<self.mins[i]:
+                        if X[i] > self.upper[i] or X[i] < self.lower[i]:
                             valid = False
-                
-                    is_in_AD.append(1*valid)
+
+                    is_in_AD.append(1 * valid)
                 else:
                     is_in_AD.append(0)
 
